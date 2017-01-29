4ab24426d4dca797f7be464b5d3d6cf5ed61a9bf
YaoShangTseng <yakushou730@gmail.com>
2017-01-29 23:06:06 +0800

    set raised exception in method

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError.new("any side of triangle should not be zero")
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError.new("any side of triangle sould not be negative")
+  elsif (a + b <= c) || (a + c <= b) || (b + c <= a)
+    raise TriangleError.new("the sum of two sides sould be larger than the rest side")
+  end
+
   if a == b && b == c
     :equilateral
   elsif (a == b && a != c) || (a == c && a != b) || (b == c && b != a)
@@ -22,6 +30,7 @@ def triangle(a, b, c)
   else
     :scalene
   end
+
 end
 
 # Error class used in part 2.  No need to change this code.
